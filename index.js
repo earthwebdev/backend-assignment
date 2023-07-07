29229eae29dac62f5193c393f029d06cc982837e
Prithvi <107529899+earthwebdev@users.noreply.github.com>
2023-07-07 10:26:40 +0545

    Feat: error handling

--- index.js
+++ index.js
@@ -42,6 +42,20 @@ app.use(apiRequestLimiter)
 
 app.use("/api/v1", indexRouter);
 
+//Error Handling for unmatched routes
+app.use((req, res, next) => {
+    const error = new Error("Page not found");
+    error.status = 404;
+    next(error);
+})
+//Error hadnler middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        status: false,
+        error: error.message,
+    })
+})
+
 const SERVER_PORT = process.env.SERVER_PORT || 8080;
 
 app.listen(SERVER_PORT, (err) => {
